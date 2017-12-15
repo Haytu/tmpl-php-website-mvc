c4e670b9b0a8bba5d34c1931483c9353a96cf95d
Haytu <clemtass84@gmail.com>
2017-12-15 14:05:59 +0100

    Ajout de vues basiques

---
+++ view/erreur.php
@@ -0,0 +1,3 @@
+<section class="row">
+    <h1>Erreur 404</h1>
+</section>
---
+++ view/navbar.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?php if(!isset($y)){ echo $pageTitle;} ?></title>
+        <link rel="stylesheet" href=""> <!-- Inclure ici votre css -->
+        <script src=""></script> <!-- Inclure ici votre js -->
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> <!-- Inclure ici votre favicon -->
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    </head>
+    <body>
